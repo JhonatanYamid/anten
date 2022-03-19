1ee4d14e43d03cdf3044a55fadc46f141431d7a0
Jhonatan Yamid Alzate <53678639+JhonatanYamid@users.noreply.github.com>
2022-03-18 20:12:42 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,8 +1,8 @@
 <?php 
-$usuario="root";
-$pass="";
-$servidor="localhost";
-$basedatos="mantenimiento";
+$usuario="uapzoo2zxbxlibuj";
+$pass="MnvnbSxmbYrjhVVZMdS0";
+$servidor="bzkvh21dd5ogyaq4v9vn-mysql.services.clever-cloud.com";
+$basedatos="bzkvh21dd5ogyaq4v9vn";
 
 $conexion= mysqli_connect($servidor, $usuario, $pass) or die ("error en el servidor");
 $bd= mysqli_select_db($conexion, $basedatos) or die ("error con la base de datos");
